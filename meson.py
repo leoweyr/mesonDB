109bc147439605e0fa731c3c18bfe05ea52cafbe
leoweyr <58080150+leoweyr@users.noreply.github.com>
2021-08-16 21:30:02 +0800

    it can work in logic

--- meson.py
+++ meson.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 #统一性操作
 def testDir(dirName):
@@ -8,32 +9,40 @@ def testDir(dirName):
         os.mkdir(dirName)
 
 #针对性操作
-def operate_testDir():
+def operate_testDir_default():
     #检测主要目录
-    testDir("database")
-    testDir("config")
+    testDir("mesonDB/database")
+    testDir("mesonDB/config")
 def operate_datebaseList(database):
     #数据库记录
-    open("config/datebaseList.config","w")
-    f = open("config/databaseList.config","a")
-    f.write(str(database))
+    open("mesonDB/config/datebaseList.config","w")
+    f = open("mesonDB/config/databaseList.config","a")
+    f.write(str(database) + "/n")
 
+#调式操作
+def timeSignINFO():
+    #时间标记器
+    main = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return "[" + main + " INFO]"
 
-#数据库操作（主程序）
-def connect(database):
-    #连接数据库
-    #检测
-    operate_testDir()
-    testDir("database/" + database)
-    operate_datebaseList(database)
-    def table(table):
-        open("database/" + database + table +".mdb","w")
-        def insert(data):
-            f = open("database/" + database + table +".mdb","a")
-            f.write(str(data) + "/n")
+version = timeSignINFO() + "meson当前版本为0.1.0"
 
+#主要操作
+class connect_default:
+    #连接数据库（默认配置）
+    def __init__(self,database):
+        operate_testDir_default()
+        self.database = database
+        testDir("mesonDB/database/" + self.database)
+        operate_datebaseList(self.database)
 
+    def table(self,db_table):
+        self.table = db_table
+        open("mesonDB/database/" + self.database + "/" + self.table +".mdb","w")
 
+    def insert(self,data):
+        f = open("mesonDB/database/" + self.database + "/"+ self.table +".mdb","a")
+        f.write(str(data) + "\n")
 
 
 
